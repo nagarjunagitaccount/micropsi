421e642b8cdabe6371a8703b1d5253ea6412b8fc
npillappa <npillappa@wealthengine.com>
2020-05-26 11:45:21 +0000

    findmany and ellucian
    git-svn-id: http://svn.wealthengine.com/svn/wecloud/test-automation/api/trunk@57497 0bf1f56a-ce14-4996-8549-8f067eef1ff9

---
+++ src/main/java/com/api/resources/jsonpath.java
@@ -0,0 +1,40 @@
+package com.api.resources;
+
+public class jsonpath
+{
+    public static String Get_total_income()
+    {
+        String res="wealth.total_income";
+        return res;
+
+    }
+    public static String Get_networth()
+    {
+        String res="wealth.networth";
+        return res;
+
+    }
+    public static String Get_accredited_investor()
+    {
+        String res="wealth.accredited_investor";
+        return res;
+
+    }
+    public static String Get_total_realestate_value()
+    {
+        String res="realestate.total_realestate_value";
+        return res;
+
+    }
+    public static String Get_total_num_properties()
+    {
+        String res="realestate.total_num_properties";
+        return res;
+
+    }
+
+
+
+
+
+}
